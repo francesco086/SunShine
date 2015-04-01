a465dec193fd97589cea5b64f3dc9d4c6b6fffef
francesco086 <francesco086@gmail.com>
2015-04-01 18:38:27 +0200

    Lesson 3 until the introduction of the blank Setting Activity

--- app/src/main/java/com/android/franceiji/sunshine/MainActivity.java
+++ app/src/main/java/com/android/franceiji/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.franceiji.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -34,8 +35,11 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //Start the activity corresponding to the Settings
+            Intent intent_open_settings = new Intent(this, SettingsActivity.class);
+            startActivity(intent_open_settings);
+
             return true;
         }
 
